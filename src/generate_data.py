dcc53efc4e58e58af6cfe5fd4181f51a549daed0
tcq1 <chu.trung@web.de>
2021-04-04 14:01:27 +0200

    implement more label functions

--- src/generate_data.py
+++ src/generate_data.py
@@ -36,7 +36,7 @@ def prepare_features(df):
     return df
 
 
-def calculate_label(last_close, close):
+def calculate_change_label(last_close, close):
     """ Calculate the label for a given input. The label is the change of the closing price from the last closing price.
 
     :param last_close: closing price of last candle
@@ -46,13 +46,35 @@ def calculate_label(last_close, close):
     return (close - last_close) / last_close
 
 
-def get_samples(time_series, feature_length=50, output_length=5):
+def calculate_up_down_label(last_close, close):
+    """ Calculate the label for a given input. The label is a boolean with True if the current closing time is higher
+    than the last closing time and False if equal or lower.
+
+    :param last_close: closing price of last candle
+    :param close: closing price of current candle
+    :return: boolean
+    """
+    return close > last_close
+
+
+def calculate_next_closing_price(last_close, close):
+    """ Return closing price
+
+    :param last_close: closing price of last candle (not necessary)
+    :param close: closing price of candle to predict
+    :return: float
+    """
+    return close
+
+
+def get_samples(time_series, feature_length=50, output_length=5, label_function=calculate_change_label):
     """ Divide time series into samples with feature_length inputs and the closing prices of the next output_length time steps being the output.
 
     The samples are of this form: [[features_0 features_1 ... features_feature_length-1], [close_feature_length+1 ... close_feature_length+output_length]]
     :param time_series: the time series as dataframe
     :param feature_length: number of time steps to use for one sample
     :param output_length: number of output values
+    :param label_function: the function that calculates the label
     :return: features, labels
     """
     features = None
@@ -64,7 +86,7 @@ def get_samples(time_series, feature_length=50, output_length=5):
         feature = [time_series[current_index:current_index + feature_length].to_numpy()]
         last_close = time_series.iloc[current_index + feature_length - 1]['close']
         close = time_series[current_index + feature_length:current_index + feature_length + output_length]['close'].to_numpy()
-        label = np.array([calculate_label(last_close, close)])
+        label = np.array([label_function(last_close, close)])
 
         if features is None:
             features = np.array(feature)
