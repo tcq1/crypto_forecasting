ae3169898d25b449d2d026c55941070a4147ee93
tcq1 <chu.trung@web.de>
2021-04-04 14:02:07 +0200

    implement autokeras

--- src/regressor.py
+++ src/regressor.py
@@ -1,27 +1,30 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import autokeras as ak
 
 from sklearn.model_selection import train_test_split
-from src.generate_data import get_samples
-from keras.layers import Input, Flatten, Dense, Dropout
-from keras import Sequential
+
+from src.generate_data import get_samples, calculate_next_closing_price
+
+from timeit import default_timer as timer
 
 
-def get_datasets(time_series_path, feature_length, output_length, train_size):
+def get_datasets(time_series_path, feature_length, output_length, train_size, label_function):
     """ Get training and validation feature and label sets.
 
     :param time_series_path: path to time series
     :param feature_length: number of past values to use for prediction
     :param output_length: number of future values to predict
     :param train_size: size of training set
+    :param label_function: function to calculate the label
     :return: x_train, x_test, y_train, y_test
     """
     # load time series
     time_series = pd.read_csv(time_series_path)
 
     # extract samples
-    features, labels = get_samples(time_series, feature_length, output_length)
+    features, labels = get_samples(time_series, feature_length, output_length, label_function)
 
     # split samples
     x_train, x_test, y_train, y_test = train_test_split(features, labels, train_size=train_size)
@@ -50,79 +53,52 @@ def normalize_data(data, mean=None, std=None):
     return data, mean, std
 
 
-def create_model(input_shape, hidden_layers, width, dropout,
-                 activation_function, output_activation, optimizer, loss, metrics):
-    """ Create the model. This is just a plain feed forward neural network.
-
-    :param input_shape: shape of input
-    :param hidden_layers: number of hidden layers
-    :param width: width of hidden layers
-    :param dropout: dropout rate
-    :param activation_function: activation function in hidden layers
-    :param output_activation: activation function of output layer
-    :param optimizer: model optimizer
-    :param loss: loss function
-    :param metrics: metrics
-    :return: model
-    """
-    model = Sequential()
-    # input layer
-    model.add(Input(shape=input_shape))
-    model.add(Flatten())
-    # hidden layers with dropout
-    for i in range(hidden_layers):
-        model.add(Dropout(dropout))
-        model.add(Dense(width, activation=activation_function))
-
-    # output layer
-    model.add(Dense(1, output_activation))
-
-    model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
-
-    return model
-
-
 def main():
-    # variables
-
+    # paths
     # path to time series
-    time_series_path = '../output/dataframes/BTCUSDT/BTCUSDT_m_05.csv'
-    # number of past values to use for prediction
-    feature_length = 50
-    # number of future values to predict
-    output_length = 1
-    # size of training set
-    train_size = 0.7
+    time_series_path = '../output/dataframes/BTCUSDT/BTCUSDT_m_15.csv'
+    # model output path
+    model_path = '../output/models/model.h5'
 
-    # model params
-    epochs = 256
-    batch_size = 32
-    dropout = 0.2
+    # data settings
+    # number of candles to use as features
+    feature_length = 25
+    # number of candles to predict
+    output_length = 1
+    # share of training data from all samples
+    train_size = 0.8
+    # label function
+    label = calculate_next_closing_price
+
+    # model settings
+    # number of models to test
+    max_trials = 300
 
     # load data sets
     print('Loading data sets...')
-    x_train, x_test, y_train, y_test = get_datasets(time_series_path, feature_length, output_length, train_size)
+    x_train, x_test, y_train, y_test = get_datasets(time_series_path, feature_length, output_length, train_size, label)
 
     # normalize data
     x_train, mean, std = normalize_data(x_train)
     x_test = normalize_data(x_test, mean, std)[0]
 
-    model = create_model(input_shape=x_train[0].shape,
-                         hidden_layers=4,
-                         width=128,
-                         dropout=dropout,
-                         activation_function='relu',
-                         output_activation='relu',
-                         optimizer='sgd',
-                         loss='mean_absolute_error',
-                         metrics=['mean_absolute_error'])
+    # flatten features
+    x_train = np.array([x_train[i].flatten() for i in range(len(x_train))])
+    x_test = np.array([x_test[i].flatten() for i in range(len(x_test))])
 
-    history = model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size, validation_data=(x_test, y_test))
+    start_time = timer()
 
-    pd.DataFrame(history.history).plot(figsize=(8, 5))
-    plt.grid(True)
-    plt.show()
-    # plt.savefig('history.png')
+    # get model
+    model = ak.StructuredDataRegressor(max_trials=max_trials)
+    model.fit(x_train, y_train)
+
+    print(f'Evaluation: {model.evaluate(x_test, y_test)}')
+
+    print(f'Done getting model after {timer() - start_time}s!')
+
+    model = model.export_model()
+    model.save(model_path)
+    print(f'Model saved in {model_path}')
 
 
 if __name__ == '__main__':
