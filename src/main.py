2786d77a41bc3a5e7942b0e14f449082f1d6a142
tcq1 <chu.trung@web.de>
2021-04-19 00:55:57 +0200

    change input paths and apply prepare features prior to prediction

--- src/main.py
+++ src/main.py
@@ -3,36 +3,31 @@ import numpy as np
 from binance.client import Client
 from keras.models import load_model
 
-from src.generate_data import calculate_next_closing_price, prepare_features, get_samples
-from src.load_candles import get_api_information, get_klines_df, add_ma
-from src.regressor import normalize_data
+from src.generate_data import prepare_features
+from src.load_candles import get_api_information, get_klines_df
 
 
 def main():
     # model
-    model_dir = '../output/models/model2'
+    model_dir = '../output/models/model'
     # load the model and normalization variables
     print('Loading model...')
     model = load_model(f'{model_dir}/model.h5')
-    mean = np.load(f'{model_dir}/mean.npy')[1:]
-    std = np.load(f'{model_dir}/std.npy')[1:]
+    mean = np.load(f'{model_dir}/mean.npy')
+    std = np.load(f'{model_dir}/std.npy')
 
     # load data to evaluate
     # number of candles to use as features
     feature_length = 50
-    # number of candles to predict
-    output_length = 1
     # interval
     interval = Client.KLINE_INTERVAL_15MINUTE
-    # label function
-    label = calculate_next_closing_price
 
     # binance client
     key, secret = get_api_information('api_key')
     client = Client(key, secret)
     df = get_klines_df(client, symbol='BTCUSDT', interval=interval, limit=feature_length + 100).astype(float)
     # calculate moving averages
-    df = add_ma(df)
+    df = prepare_features(df)
     # only take feature_length values
     df = df[-50:]
     # prepare features
